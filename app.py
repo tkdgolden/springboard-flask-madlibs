b686c311a6150bf7726e0ebf0733d07d18eca96a
Olivia Golden <olivia.golden.w@gmail.com>
2023-11-08 18:49:40 -0600

    test

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template, redirect
+# from flask_debugtoolbar import DebugToolbarExtension
+from stories import story_list, get_story
+
+app = Flask(__name__)
+# app.config['SECRET_KEY'] = "oh-so-secret"
+
+
+# debug = DebugToolbarExtension(app)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == 'GET':
+        return render_template("index.html", stories=story_list)
+    if request.method == 'POST':
+        story_id = request.form["story"]
+        print(story_id)
+        return redirect(f'/story/{story_id}')
+
+@app.route('/story/<story_id>', methods=["GET", "POST"])
+def story(story_id):
+    if request.method == 'GET':
+        # story_id = request.args["story"]
+        print(story_id)
+        # find the story object in the story list
+        this_story = get_story(story_id)
+        
+        # send the prompts to a template to make a form out of
+        return render_template("story_form.html", story=this_story)
+    
+    if request.method == 'POST':
+        form_data = request.form
+        answers = {}
+        for each in form_data:
+            answers[each] = form_data[each]
+        print(answers)
+
+        this_story = get_story(story_id)
+        result = this_story.generate(answers)
+
+        return render_template("view_story.html", result=result)
+    
+@app.route('/test/<int:story_id>')
+def test(story_id):
+    print(story_id)
+    return render_template("index.html")
--- stories.py
+++ stories.py
@@ -1,6 +1,5 @@
 """Madlibs Stories."""
 
-
 class Story:
     """Madlibs story.
 
@@ -18,11 +17,13 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, words, text, title, story_id):
         """Create story with words and template text."""
 
         self.prompts = words
         self.template = text
+        self.title = title
+        self.id = story_id
 
     def generate(self, answers):
         """Substitute answers into text."""
@@ -34,12 +35,24 @@ class Story:
 
         return text
 
-
 # Here's a story to get you started
-
-
-story = Story(
+story_1 = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+       large {adjective} {noun}. It loved to {verb} {plural_noun}.""", "Long Ago", 0
 )
+
+story_2 = Story(
+    ["noun", "place", "verb"], """Have you ever heard why the {noun} crossed the {place}?  It was trying to {verb}.""", "Crossing", 1
+)
+
+story_3 = Story(
+    ["superlative_adjective", "color", "noun", "ing_verb"], """The {superlative_adjective} thing I've ever seen was a {color} {noun} {ing_verb} down by the Sea!""", "Down by the Sea", 2
+)
+
+story_list = [story_1, story_2, story_3]
+
+def get_story(story_id):
+    for each in story_list:
+        if each.id == int(story_id):
+            return each
